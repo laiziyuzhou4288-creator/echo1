03640adc949ce251c2855399170f16b808491437
laiziyuzhou4288-creator <laiziyuzhou4288@gmail.com>
2026-02-14 11:19:55 +0800

    Update supabaseClient.ts

--- services/supabaseClient.ts
+++ services/supabaseClient.ts
@@ -1,7 +1,7 @@
-
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://zzhbhyfqsfmobrzgergi.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_Ui-TgVfospVWZAfTyR5YEA_UFEHi48O';
+// 使用 import.meta.env 读取 Vite 环境变量
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
